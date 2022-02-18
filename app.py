e6f860144ad4ec43dfc7ce870501c88d480e4bb3
alsomeb <alsomeb@gmail.com>
2022-02-18 09:15:35 +0100

    migration

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from website import create_app
 from website.models import seedData
+from flask_migrate import upgrade
+
 app, migrate = create_app()
 
 if __name__  == "__main__":
-    seedData()
+    with app.app_context():
+        upgrade()
+        seedData()
     app.run(debug=True)
