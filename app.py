7cc37c8eb3da54bea109fbdacb64e34d64ab8104
Filip Lindberg <filiplindberg@hotmail.se>
2022-02-18 09:16:36 +0100

    bajs

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from msilib.schema import Upgrade
 from website import create_app
 from website.models import seedData
 app, migrate = create_app()
 
 if __name__  == "__main__":
+    
     seedData()
     app.run(debug=True)
